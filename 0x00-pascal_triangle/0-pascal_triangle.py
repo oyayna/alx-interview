f25b3da472083bc89c2f9746ff08faa41e45a40f
amir-ee <melio@kali>
2024-06-03 16:17:00 +0100

    Update

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Pascal Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+      Pascal Triangle
+      Return a list of
+      Integers
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for _ in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+        for j in range(1, len(prev_row)):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
