9734b3cfe3fc37769ec00e7e8651449f141e2133
Kamado Tanjiro <melio@kali>
2024-07-27 20:29:37 -0400

    Update

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" File 0-making_change"""
+
+
+def makeChange(coins, total):
+    """ Generate changes needed to reach total amount """
+    if total <= 0:
+        return 0
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
