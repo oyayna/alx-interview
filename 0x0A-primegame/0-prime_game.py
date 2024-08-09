13a35e621fb32d04a0b9f4a4a6ebfede3058f6e6
Kamado Tanjiro <melio@kali>
2024-08-08 20:32:47 -0400

    Update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -10,31 +10,31 @@ def isWinner(x, nums):
     if x != len(nums):
         return None
 
-    ben_score = 0
-    maria_score = 0
+    ben = 0
+    maria = 0
 
-    primes = [1 for _ in range(max(nums) + 1)]
+    primes = [1 for _ in range(sorted(nums)[-1] + 1)]
     primes[0], primes[1] = 0, 0
     for i in range(2, len(primes)):
-        remove_multiples(primes, i)
+        rm_multiples(primes, i)
 
     for num in nums:
         if sum(primes[:num + 1]) % 2 == 0:
-            ben_score += 1
+            ben += 1
         else:
-            maria_score += 1
+            maria += 1
 
-    if ben_score > maria_score:
+    if ben > maria:
         return "Ben"
-    if maria_score > ben_score:
+    if maria > ben:
         return "Maria"
     return None
 
-def remove_multiples(primes, x):
+def rm_multiples(ls, x):
     """Removes multiples of primes"""
-    for i in range(2, len(primes)):
+    for i in range(2, len(ls)):
         try:
-            primes[i * x] = 0
+            ls[i * x] = 0
         except (ValueError, IndexError):
             break
 
