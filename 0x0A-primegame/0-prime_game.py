1816bb6389b7517b7d76d068ed303ab76271b33f
Kamado Tanjiro <melio@kali>
2024-08-08 20:38:17 -0400

    Update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,42 +1,44 @@
 #!/usr/bin/python3
-"""0. Prime Game - Maria and Ben are playing a game"""
-
+"""0. Prime Game - Maria and Ben are playing a game."""
 
 def isWinner(x, nums):
-    """x - rounds
-    nums - numbers list
+    """Determine the winner of the Prime Game.
+
+    Args:
+        x (int): Number of rounds.
+        nums (list): List of numbers.
+
+    Returns:
+        str: The winner ("Ben" or "Maria"), or None if no winner.
     """
-    if x <= 0 or nums is None:
-        return None
-    if x != len(nums):
+    if x <= 0 or nums is None or x != len(nums):
         return None
 
-    ben = 0
-    maria = 0
+    ben, maria = 0, 0
 
-    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a = [1 for _ in range(sorted(nums)[-1] + 1)]
     a[0], a[1] = 0, 0
+
     for i in range(2, len(a)):
         rm_multiples(a, i)
 
     for i in nums:
-        if sum(a[0:i + 1]) % 2 == 0:
+        if sum(a[:i + 1]) % 2 == 0:
             ben += 1
         else:
             maria += 1
+
     if ben > maria:
         return "Ben"
-    if maria > ben:
+    elif maria > ben:
         return "Maria"
     return None
 
-
 def rm_multiples(ls, x):
-    """removes multiple
-    of primes
-    """
+    """Removes multiples of primes."""
     for i in range(2, len(ls)):
         try:
             ls[i * x] = 0
         except (ValueError, IndexError):
             break
+
