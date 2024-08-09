67d055ab42dcc5dc43b9f56f622df06b08ae1109
Kamado Tanjiro <melio@kali>
2024-08-08 20:40:59 -0400

    Update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """0. Prime Game - Maria and Ben are playing a game"""
 
+
 def isWinner(x, nums):
     """x - rounds
-    nums - numbers list"""
-    if x <= 0 or nums is None:
+    nums - numbers list
+    """
+    if x <= 0 or not nums :
         return None
     if x != len(nums):
         return None
@@ -12,29 +14,29 @@ def isWinner(x, nums):
     ben = 0
     maria = 0
 
-    a = [1 for _ in range(sorted(nums)[-1] + 1)]
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
     a[0], a[1] = 0, 0
-
     for i in range(2, len(a)):
         rm_multiples(a, i)
 
     for i in nums:
-        if sum(a[:i + 1]) % 2 == 0:
+        if sum(a[0:i + 1]) % 2 == 0:
             ben += 1
         else:
             maria += 1
-
     if ben > maria:
         return "Ben"
     if maria > ben:
         return "Maria"
     return None
 
+
 def rm_multiples(ls, x):
-    """removes multiple of primes"""
+    """removes multiple
+    of primes
+    """
     for i in range(2, len(ls)):
         try:
             ls[i * x] = 0
         except (ValueError, IndexError):
             break
-
